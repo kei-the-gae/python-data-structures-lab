9b2d7cde34cfc50f6c8222a193886fd4a017db3c
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-09 00:49:39 -0700

    Add exercise 1

--- exercises.py
+++ exercises.py
@@ -14,3 +14,18 @@ def example_list_function():
         print(element)
 # Call the function and print each element
 example_list_function()
+
+# Exercise 1: List and Indexing
+#
+# Create a list named students containing at least three student names (strings).
+# Assign the second student’s name to a variable named first_student.
+# Assign the last student’s name to a variable named last_student.
+
+def manage_students():
+    # your code here
+    student_names = ['Kei', 'Avery', 'Kayleigh']
+    first_student = student_names[1]
+    last_student = student_names[-1]
+    return f'first_student: {first_student}, last_student: {last_student}'
+# Call the function and print the result
+print('Exercise 1:', manage_students())
